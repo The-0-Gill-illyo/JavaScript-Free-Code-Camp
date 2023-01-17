ebc8d87a781c8894c1c01d1861ba3b9c4496648c
The-0-Gill-illyo <theogill1369@gmail.com>
2023-01-17 18:34:50 -0500

    Completed 13 intro to JS Free Code Camp JS Data Structures and Algorithms

---
+++ Problem1.js
@@ -0,0 +1,15 @@
+/*
+Comment Your JavaScript Code
+Comments are lines of code that JavaScript will intentionally ignore. Comments are a great way to leave notes to yourself and to other people who will later need to figure out what that code does.
+
+There are two ways to write comments in JavaScript:
+
+Using // will tell JavaScript to ignore the remainder of the text on the current line.
+This is an in-line comment:
+*/
+
+/* This is my
+multi-line comment 
+in Free Code Camp!*/
+
+// I'm Sorry I should have read the instructions for a single line comment only!
